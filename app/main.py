a2f7a21081ced8ec1874c8a1e000220e3db4f09e
Marwane <marwaneben@mbp-van-marwane.home>
2025-05-10 14:59:20 +0200

    céation du script python, base noSQL, deux conteneurs qui communiquent via docker, ajout GitHub actions pour CI/CD

---
+++ app/main.py
@@ -0,0 +1,31 @@
+import random
+import time
+from datetime import datetime
+from faker import Faker
+from pymongo import MongoClient
+
+print("!!!!!! demarrage du script Python log-generator !!!!!!")
+
+fake = Faker()
+
+client = MongoClient("mongodb://mongo:27017/")
+db = client["logdb"]
+collection = db["logs"]
+
+levels = ["INFO", "DEBUG", "WARNING", "ERROR", "CRITICAL"]
+
+def generate_log():
+    log = {
+        "time": datetime.utcnow().isoformat(),
+        "level": random.choice(levels),
+        "user": fake.user_name(),
+        "action": fake.sentence(nb_words=6),
+        "ip": fake.ipv4()
+    }
+    return log
+
+while True:
+    log = generate_log()
+    collection.insert_one(log)
+    print(f"Log enregistré : {log}")
+    time.sleep(3)
